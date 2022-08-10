ebc4307c121d5775ecedcfdfbb576acf83747e74
Christopher Reif <christopher94.reif@gmail.com>
2022-08-10 12:01:09 -0500

    Created Hello World

---
+++ Java/Java_Fundamentals/src/main/java/Galvanized/Java_Fundamentals.java
@@ -0,0 +1,10 @@
+package Galvanized;
+
+public class Java_Fundamentals {
+    // You can use psvm as a shortcut
+    public static void main(String[] args) {
+        // sout is a shortcut
+        System.out.println("Hello World");
+    }
+}
+
--- galvanized/Galvanized/CodeAlong8_10_2022.java
+++
@@ -1,5 +0,0 @@
-public class CodeAlong8_10_2022 {
-    public static void main(String[] args){
-        System.out.println("Hello, Cohort4")
-    }
-}

a102f2b409057956eee10d6e47ccb087686cc112
Christopher Reif <35343166+creif94@users.noreply.github.com>
2022-08-29 14:35:15 -0500

    Updated

---
+++ HotelRoomReservation/src/main/java/model/FreeRoom.java
@@ -0,0 +1,11 @@
+package model;
+
+public class FreeRoom extends Room{
+
+    public FreeRoom(){
+        this.price = 0.0;
+
+
+    }
+
+}
---
+++ HotelRoomReservation/src/main/java/model/Room.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Room implements IRoom {
+    private String roomNumber;
+    Double price;
+    RoomType enumeration;
+
+
+    @Override
+    public String getRoomNumber() {
+        return null;
+    }
+
+    @Override
+    public Double getRoomPrice() {
+        return null;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return null;
+    }
+
+    @Override
+    public boolean isFree() {
+        return false;
+    }
+}
